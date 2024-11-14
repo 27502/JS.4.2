529f05acf1411459b3a5768c707844cea0019284
Habibullo <rahmatullayevhabibullo6@gmail.com>
2024-11-14 16:52:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,166 @@
+// -------------------1-misol-------------------------------------------------
+{
+//     function greeting(name) {
+//     return `Assalomu alaykum, ${name}!`;
+// }
+// console.log(greeting("Habibullo")); 
+
+// const greeting = function(name) {
+//     return `Assalomu alaykum, ${name}!`;
+// };
+// const greeting = (name) => {
+//     return `Assalomu alaykum, ${name}!`;
+// };
+}
+
+
+
+
+// -------------------2-misol-------------------------------------------------
+{
+    // function rangeWithWhileLoop(a, b) {
+    //     const result = [];
+    //     let i = a;
+    //     while (i <= b) {
+    //         result.push(i);
+    //         i++;
+    //     }
+    //     return result;
+    // }
+    
+    // console.log(rangeWithWhileLoop(3, 6)); 
+
+
+
+    // const rangeWithWhileLoop = function(a,b)
+    // {
+    //     const result = [];
+    //     let i = a;
+    //     while (i <= b) {
+    //         result.push(i);
+    //         i++;
+    //     }
+    //     return result;
+    // }
+    // console.log(rangeWithWhileLoop(3, 6));
+
+
+
+
+    // const rangeWithWhileLoop = (a,b) => {
+    //     const result = [];
+    //     let i = a;
+    //     while (i <= b) {
+    //         result.push(i);
+    //         i++;
+    //     }
+    //     return result;
+    // }
+    
+}
+
+// -------------------3-misol-------------------------------------------------
+{
+    // function bl(a) {
+    //     if (a === true) {
+    //         return 1;
+    //     } else {
+    //         return 0;
+    //     }
+    // }
+    
+    // console.log(bl(true)); 
+    // console.log(bl(false));
+
+
+    // const bl = function(a) {
+    //     if (a === true) {   
+    //         return 1;
+    //     } else {
+    //         return 0;
+    //     }
+    // }
+    // console.log(bl(true));
+    // console.log(bl(false));
+
+
+
+    // const bl = (a) => {
+    //     if (a === true) {
+    //         return 1;
+    //     } else {    
+    //         return 0;
+    //     }
+    // }    
+    // console.log(bl(true));
+    // console.log(bl(false));
+    
+}
+
+// -------------------4-misol-------------------------------------------------
+
+{
+//  function getFirstLetter(name) {
+//     return name[0];
+// }
+
+// console.log(getFirstLetter("John")); 
+// console.log(getFirstLetter("Alice")); 
+
+
+
+
+// const getFirstLetter = function(name) {
+//     return name[0];
+// }
+// console.log(getFirstLetter("John")); 
+// console.log(getFirstLetter("Alice"));
+
+// const getFirstLetter = (name) => {
+//     return name[0];
+// }
+// console.log(getFirstLetter("John")); 
+// console.log(getFirstLetter("Alice"));
+   
+}
+
+
+// -------------------5-misol-------------------------------------------------
+
+    {
+//      function sum(...numbers) {
+//     let total = 0;
+//     for (let num of numbers) {
+//       total += num;
+//     }
+//     return total;
+//   }
+  
+//   console.log(sum(1, 2, 3, 4, 5));
+//   console.log(sum(10, 20, 30));   
+  
+
+   
+//   const sum = (...numbers) => {
+//     let total = 0;
+//     for (let num of numbers) {
+//       total += num;
+//     }
+//     return total;
+//   }
+
+//   console.log(sum(1, 2, 3, 4, 5));  
+//   console.log(sum(10, 20, 30));
+
+
+
+//    const sum = (...numbers) => {
+//     let total = 0;
+//     for (let num of numbers) {
+//       total += num;
+//     }
+//     return total;
+//   }
+//   console.log(sum(1, 2, 3, 4, 5));
+//   console.log(sum(10, 20, 30));
+    }
